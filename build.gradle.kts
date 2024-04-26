ad161e2fe46e96f3c4ea9f47c80f105944b71be0
Gregory Mitchell <grmitchell713@gmail.com>
2024-04-26 13:32:39 -0500

    Bump `publish-plugin` to 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.23"
     id("org.jetbrains.dokka") version "1.9.20"
-    id("com.gradle.plugin-publish") version "1.1.0"
+    id("com.gradle.plugin-publish") version "1.2.1"
     signing
 }
 
